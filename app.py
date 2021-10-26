034b14ac43616e2c3b9fa85cd9ba3dd28733cc8f
AdamKealy <C00213914@itcarlow.ie>
2021-10-26 16:05:44 +0100

    get methods working

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def display_form():
                            heading="Please fill in this form",)
 
 
-@app.post("/processform")
+@app.post("/processfeedback")
 def save_data():
     """
         Receive the data from the HTML form, then save it to a disk file, then respond
@@ -31,13 +31,10 @@ def save_data():
         The following inputs are expected: first, last, and dob.
     """
     # python-name = html-name:
-    the_first = request.form["first"]
-    the_last = request.form["last"] 
-    the_dob = request.form["dob"] 
+    the_feedback = request.form["feedback"]
     # So... now, use the python-names in your code:
-    with open("suckers.txt", "a") as sf:
-        print(f"{the_first}, {the_last}, {the_dob}", file=sf)
-    return f"Thanks, {the_first}, we promise not to sell your data to the bad guys."    
+    with open("comments.txt", "a") as sf:
+        print(f"{the_feedback}", file=sf) 
 
 
 if __name__ == "__main__":
