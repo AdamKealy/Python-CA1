1bbe02b0e01cbd68c4b978e9bf9926fc63cbe841
AdamKealy <C00213914@itcarlow.ie>
2021-10-26 19:12:12 +0100

    Added contents and links to the computing page

--- app.py
+++ app.py
@@ -14,31 +14,32 @@ def index():
 def home():
     return render_template("home.html",
                             title="Home Page",
-                            heading="",)
+                            heading="Home Page",)
 
 @app.get("/showPersonal")
 def personal():
     return render_template("personal.html",
                             title="Personal Page",
-                            heading="",)
+                            heading="Personal Page",)
 
 @app.get("/showCV")
 def cv():
     return render_template("cv.html",
                             title="CV Page",
-                            heading="",)
+                            heading="CV Page",)
 
 @app.get("/showComputing")
 def computing():
     return render_template("computing.html",
                             title="Computing Page",
-                            heading="",)
+                            heading="Computing Page",)
 
 @app.get("/showInterests")
 def interests():
     return render_template("interests.html",
                             title="Interests Page",
-                            heading="",)
+                            heading="Interests Page",)
+
                             
 @app.post("/processfeedback")
 def save_data():
